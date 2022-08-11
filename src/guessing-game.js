9a3cb8f59fd6c9ffb9570cb8fdf47b065162426a
vigo44 <vvigo@rambler.ru>
2022-08-12 00:46:24 +0400

    add: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min=min;
+      this.max=max;
     }
 
     guess() {
-
+       this.middle=(((this.max+this.min)/2 + 0.5)^0);
+       return (this.middle);
     }
 
     lower() {
-
+      this.max=this.middle;
+      
     }
+    
 
     greater() {
-
+      this.min=this.middle;
+      
     }
 }
 
